0cabb18c5a04d22e8848e55c6dfc3deba8f20196
Arcaticas <theo.grant9@gmail.com>
2019-12-02 05:50:42 -0600

    Guardian Upgrade

--- Phoenix-of-the-Forest/Assets/Scripts/Player/GuardianScript.cs
+++ Phoenix-of-the-Forest/Assets/Scripts/Player/GuardianScript.cs
@@ -13,59 +13,71 @@ public class GuardianScript : MonoBehaviour {
 
    bool flipX = false;
 
-   [SerializeField] float hoverOffest;
-   Vector3 hoverOffestVector;
+    [SerializeField] float hoverOffest;
+    Vector3 hoverOffestVector;
+    
 
    // Start is called before the first frame update
    void Start() {
-      platX = false;
-      enemyX = false;
-      hoverOffestVector = new Vector3(0, hoverOffest, 0);
+        platX = false;
+        enemyX = false;
+        hoverOffestVector = new Vector3(0, hoverOffest, 0);
+        
    }
 
    // Update is called once per frame
    void Update() {
-      Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
-      Vector2 targetVelocity = champ.transform.position + hoverOffestVector - transform.position;
-      rb.velocity = targetVelocity * 3.0f;
+        Vector2 targetVelocity = champ.transform.position + hoverOffestVector - transform.position;
+        rb.velocity = targetVelocity * 3.0f;
 
-      flipX = transform.position.x < champ.transform.position.x;
-      GetComponentInChildren<SpriteRenderer>().flipX = flipX;
+        flipX = transform.position.x < champ.transform.position.x;
+        GetComponentInChildren<SpriteRenderer>().flipX = flipX;
 
-      if (Input.GetMouseButtonDown(0) && !enemyX && !platX) {
-         platX = true;
-         plat1 = Instantiate(Resources.Load<GameObject>("Platform_Prefabs/platform_vines"));
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         plat1.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        
 
-         StartCoroutine(Kill());
-      }
+
+
+        if (Input.GetMouseButtonDown(0) && !enemyX && !platX)
+        {
+            platX = true;
+            float randomNumber = Random.Range(1, 3);
+            GetComponentInChildren<GuardianTimer>().Platform();
+
+            if (randomNumber == 1)
+            {
+                plat1 = Instantiate(Resources.Load<GameObject>("Platform_Prefabs/platform_vines"));
+            }
+
+            if (randomNumber == 2)
+            {
+                plat1 = Instantiate(Resources.Load<GameObject>("Platform_Prefabs/platform_novines"));
+            }
+
+            if (randomNumber == 3)
+            {
+                plat1 = Instantiate(Resources.Load<GameObject>("Platform_Prefabs/platform_nograss"));
+            }
+
+
+            plat1.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+
+            StartCoroutine(Kill());
+        }
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Mathf.Infinity, 10))
+        {
+            Debug.Log("cunt");
+        }
+
    }
 
-   void OnTriggerEnter2D(Collider2D col) {
-      if (col.gameObject.tag == "Enemy") {
-         enemyX = true;
-      }
-   }
-
-   private void OnTriggerExit2D(Collider2D col) {
-      if (col.gameObject.tag == "Enemy") {
-         enemyX = false;
-      }
-   }
-
-   void OnTriggerStay2D(Collider2D col) {
-
-      if (col.gameObject.tag == "Enemy" && Input.GetMouseButtonDown(0)) {
-         col.GetComponentInParent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-         StartCoroutine(Unfreeze(col));
-      }
 
 
-
-   }
+   
    IEnumerator Kill() {
       yield return new WaitForSeconds(1.5f);
       plat2 = plat1;
