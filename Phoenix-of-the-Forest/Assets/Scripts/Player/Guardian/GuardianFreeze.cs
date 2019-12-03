62a67692a13f19c36292334bdaa5c208f8314fa2
Arcaticas <theo.grant9@gmail.com>
2019-12-03 15:05:12 -0600

    FreezeUpdates

---
+++ Phoenix-of-the-Forest/Assets/Scripts/Player/Guardian/GuardianFreeze.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianFreeze : MonoBehaviour
+{
+
+    bool enemyx;
+    Collider2D freezeBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyx = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) && enemyx)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            enemyx = true;
+            freezeBox = col;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enemyx = false;
+    }
+}
