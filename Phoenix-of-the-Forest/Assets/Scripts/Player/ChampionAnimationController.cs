a0750e4bbfc5c4a9afa72137da8a1afe483f6de9
Arcaticas <theo.grant9@gmail.com>
2019-12-05 14:48:26 -0600

    Animations on Level

---
+++ Phoenix-of-the-Forest/Assets/Scripts/Player/ChampionAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChampionAnimationController : MonoBehaviour {
+   Animator anim;
+
+   PlayerMovement playerMovement;
+   Player player;
+
+   void Start() {
+      anim = GetComponent<Animator>();
+
+      playerMovement = GetComponentInParent<PlayerMovement>();
+      player = GetComponentInParent<Player>();
+   }
+
+
+   void Update() {
+      anim.SetBool("isGrounded", playerMovement.grounded);
+
+      anim.SetBool("isAttacking", player.isAttacking);
+
+      anim.SetBool("isWalking", playerMovement.isWalking);
+   }
+}
